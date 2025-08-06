fddb508084bcca8185d67e2c5f386514a83d610e
Isadora Goedert <99146463+isagrt@users.noreply.github.com>
2025-08-06 08:30:34 -0300

    Add files via upload

---
+++ janela.java
@@ -0,0 +1,113 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class janela {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Poupex");
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    /* 
+        JPanel principal = new JPanel();
+        principal.setBackground(new Color(171, 171, 171));
+        principal.setLayout(new BorderLayout(10, 10));
+    */ 
+        
+        //Painel com os labels
+        JPanel text = new JPanel();
+        text.setBackground(new Color(171, 171, 171));
+        text.setBorder(BorderFactory.createLineBorder(Color.black, 2));
+        text.setLayout(new GridBagLayout());
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(15, 1, 15, 5);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        
+        //juros
+        JLabel fees = new JLabel("Juros ao mes %: ");
+        fees.setFont(null);// arrumar estilização
+        JTextField feesField = new JTextField();
+        feesField.setPreferredSize(new Dimension(150, 25));
+
+        text.add(fees, gbc);
+        gbc.gridx = 1; // label na coluna (horizontal)
+        text.add(feesField, gbc);  
+        gbc.gridy++; // avança para a próxima linha
+        gbc.gridx = 0; // Volta a coluna para 0 
+
+        //anos
+        JLabel years = new JLabel("N° de anos: ");
+        years.setFont(null);// arrumar estilização
+        JTextField yearsField = new JTextField();
+        yearsField.setPreferredSize(new Dimension(150, 25));
+
+        text.add(years, gbc);
+        gbc.gridx = 1;
+        text.add(yearsField, gbc);
+        gbc.gridy++;
+        gbc.gridx = 0;
+
+        //deposito mensal
+        JLabel deposit = new JLabel("Depósito mensal R$: ");
+        deposit.setFont(null); //arrumar estilização
+        JTextField depositField = new JTextField();
+        depositField.setPreferredSize(new Dimension(150, 25));
+
+        text.add(deposit, gbc);
+        gbc.gridx = 1;
+        text.add(depositField, gbc);
+        gbc.gridy++;
+        gbc.gridx = 0;
+
+        JLabel result = new JLabel("Total poupado R$: ");
+        result.setFont(null); //arrumar estilização
+        JTextField resultField = new JTextField();
+        resultField.setPreferredSize(new Dimension(150, 25));//necessário definir um setText ao campo de resultado
+
+        text.add(result, gbc);
+        gbc.gridx = 1;
+        text.add(resultField, gbc);
+        gbc.gridy++;
+        gbc.gridx = 0;
+
+        JButton ok = new JButton("OK");
+        ok.setPreferredSize(new Dimension(150, 25));
+        ok.addActionListener(e -> {
+            double juros = Double.parseDouble(feesField.getText().trim()) / 100.0;
+            int anos = Integer.parseInt(yearsField.getText().trim());
+            int meses = anos *12;
+            String convert = depositField.getText().trim();
+            double deposito = Double.parseDouble(convert);
+            double resultado = 0.0;
+
+            for (int i = 0; i< meses; i++){
+                resultado += deposito;
+                resultado += resultado * juros;
+            }        
+            resultField.setText(String.format("%.2f", resultado));
+            resultField.setEditable(false);
+        });
+        
+        gbc.gridx = 0;
+        gbc.gridwidth = 2; 
+        text.add(ok, gbc);
+        gbc.gridwidth = 1; 
+
+        frame.setLayout(new BorderLayout());
+        frame.add(text, BorderLayout.CENTER);
+        frame.setVisible(true);
+    }
+}
+/*
+        double jurostotal = Double.parseDouble(txtJuros.getText()) / 100.0;
+            int anos = Integer.parseInt(txtAnos.getText());
+            double deposito = Double.parseDouble(txtDeposito.getText());
+            int meses = anos * 12;
+            double total = 0.0;
+ 
+            for (int i = 0; i < meses; i++) {
+                total += deposito;
+                total += total * jurostotal;
+ */
